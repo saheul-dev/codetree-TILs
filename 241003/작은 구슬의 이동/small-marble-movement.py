e7639a8981f43a5da02ea69d9f2c4876f164d2b2
chanwoo ji <saheul.dev@gmail.com>
2024-10-03 18:47:05 +0900

    Title: 작은 구슬의 이동, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/작은 구슬의 이동/small-marble-movement.py
+++ 241003/작은 구슬의 이동/small-marble-movement.py
@@ -1,5 +1,3 @@
-from enum import Enum
-
 n, t = map(int, input().split())
 r, c, d = input().split()
 r, c = int(r), int(c)
@@ -7,7 +5,7 @@ r, c = int(r), int(c)
 dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
 nx, ny = c, r
 
-Dir_dic = {'R':0, 'U':1, 'D':2, 'L':3}
+Dir_dic = {'R':0, 'D':1, 'U':2, 'L':3}
 dir_num = Dir_dic[d]
 
 while(t > 0):
@@ -28,6 +26,5 @@ while(t > 0):
         dir_num = 3 - dir_num
 
     t -= 1
-
 
 print(ny, nx)
